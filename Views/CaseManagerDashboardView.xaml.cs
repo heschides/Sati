d4ccb8a3443f97914c2a4507655b334f9ff9e001
Joshua White <Joshua.White@robinbradleyams.com>
2026-04-18 16:28:41 -0400

    - CaseManagerDashboardView is now a thin container with sub-nav strip - Sub-nav routes to Overview, Clients, Notes, Caseload Matrix - CaseManagerDashboardContentView holds the dashboard content - DataGrid_MouseDoubleClick moved to CaseManagerDashboardContentView code-behind - All converters moved to App.xaml for global UserControl access

--- Views/CaseManagerDashboardView.xaml.cs
+++ Views/CaseManagerDashboardView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Sati.ViewModels;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows.Controls;
 
 namespace Sati.Views
 {
@@ -9,12 +7,6 @@ namespace Sati.Views
         public CaseManagerDashboardView()
         {
             InitializeComponent();
-        }
-
-        private void DataGrid_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            if (DataContext is CaseManagerDashboardViewModel vm && vm.SelectedNote is not null)
-                vm.EnterEditMode();
         }
     }
 }
